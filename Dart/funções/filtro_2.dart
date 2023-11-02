c2125cb358af41ebc2aad29e20c676dc69e4e43c
Kessy Godoy <kessyon@gmail.com>
2023-11-02 13:33:55 -0300

    where feito manualmente

--- Dart/funções/filtro_2.dart
+++ Dart/funções/filtro_2.dart
@@ -1,15 +1,23 @@
+List<E> filtrar<E>(List<E> lista, bool Function(E) fn) {
+  List<E> listaFiltrada = [];
+  for (E elemento in lista) {
+    if (fn(elemento)) {
+      listaFiltrada.add(elemento);
+    }
+  }
+  return listaFiltrada;
+}
+
 void main(List<String> args) {
-  var notas = [8.2, 7.1, 6.2, 4.4, 3.9, 8.8, 9.1, 5.1];
+  var notas = [8.2, 7.3, 6.8, 5.4, 2.7, 9.3];
+  var boasNotasFn = (double nota) => nota >= 7.5;
 
-  bool Function(double) notasBoasFn = (double nota) => nota >= 7;
-  var notasMuitoBoasFn = (double nota) => nota >= 8.8;
+  var somenteNotasBoas = filtrar(notas, boasNotasFn);
 
-  var notasBoas = notas.where(notasBoasFn);
-  var notasMuitoBoas = notas.where(notasMuitoBoasFn);
-  var notasMaisMelhoresAinda = notas.where((n) => n >= 9);
+  print(somenteNotasBoas);
 
-  print(notas);
-  print(notasBoas);
-  print(notasMuitoBoas);
-  print(notasMaisMelhoresAinda);
+  var nomes = ['Ana', 'Bia', 'Rebeca', 'Kessy', 'Joao'];
+  var nomesGrantesFn = (String nome) => nome.length >= 4;
+
+  print(filtrar(nomes, nomesGrantesFn));
 }
