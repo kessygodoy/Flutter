eeb3418d0e2c8479036dbac0150500a1db790987
Kessy Godoy <kessyon@gmail.com>
2023-11-02 09:23:04 -0300

    Funcao como parametro

---
+++ Dart/funções/funcao_em_variavel copy.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  int a = 2;
+  // tipo nome = valor;
+  int Function(int, int) soma1 = somaFn;
+  print(soma1(20, 313));
+
+  var soma2 = (x, y) {
+    // FUNÇÃO ANONIMA
+    return x + y;
+  };
+
+  print(soma2(2, 4));
+}
+
+int somaFn(int a, int b) {
+  return a + b;
+}
