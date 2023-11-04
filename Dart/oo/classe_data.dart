870d1741098a097d929617543c85b7068809eab1
Kessy Godoy <kessyon@gmail.com>
2023-11-04 07:47:16 -0300

    import

--- Dart/oo/classe_data.dart
+++ Dart/oo/classe_data.dart
@@ -10,7 +10,15 @@ class Data {
   // }
   //Colocando o this nos parametros vc automaticamente diz que o construtor
   // declara eles como os que vao ser recebidos na instanciação
+
   Data([this.dia = 1, this.mes = 1, this.ano = 1]);
+
+  //Construtores Nomeados
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1970});
+  Data.ultimoDiaDoAno(this.ano) {
+    dia = 31;
+    mes = 12;
+  }
 
   String obterFormatada() {
     return "$dia/$mes/$ano";
@@ -41,4 +49,16 @@ void main(List<String> args) {
 
   print(dataAniversario);
   print(dataCompra);
+
+  print(new Data());
+  print(Data(31));
+  print(Data(31, 12));
+  print(Data(31, 12, 2021));
+
+  print(Data.com(ano: 2022));
+
+  var dataFinal = Data.com(dia: 12, mes: 7, ano: 2024);
+  print("O Mickey será público em $dataFinal");
+
+  print(Data.ultimoDiaDoAno(2023));
 }
---
+++ Dart/oo/test/principal.dart
@@ -0,0 +1,9 @@
+import '../model/pessoa.dart';
+
+void main(List<String> args) {
+  var p1 = Pessoa();
+
+  p1.nome = "Guesbo";
+
+  print("O nome da pessoa é ${p1.nome}");
+}
