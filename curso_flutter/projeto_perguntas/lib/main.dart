9d552c886d715ce59a03bc72cdb38cb91ced604e
Kessy Godoy <kessyon@gmail.com>
2023-11-05 10:37:09 -0300

    Usando StatefulWidget

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(PerguntaApp());
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            TextButton(onPressed: responder, child: Text('Resposta 1')),
+            TextButton(onPressed: responder, child: Text('Resposta 2')),
+            TextButton(onPressed: responder, child: Text('Resposta 3')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+}
